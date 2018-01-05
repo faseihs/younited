3975d3541d1b95a54a69d23356e246c26aa75b1e
Faseih Saad <fsaad.bese15seecs@seecs.edu.pk>
2018-01-05 21:45:48 +0500

    Another conversion

--- profile.php
+++ profile.php
@@ -1,111 +1,284 @@
-<?php
-
-include_once('database.php');
-$message = "";
-session_start();
-if(isset($_SESSION['id']) && isset($_SESSION['name']))
-{
-	$name = $_SESSION['name'];
-	$id = $_SESSION['id'];
-
-	$query = mysqli_query($con,"SELECT * FROM users WHERE id = '$id'");
-	$row = mysqli_fetch_assoc($query);
-	$email = $row["email"];
-	$dob = $row["dob"];
-	$query1 = mysqli_query($con,"SELECT * FROM profile WHERE id = '$id'");
-	$row1 = mysqli_fetch_assoc($query1);
-	$profile_pic = $row1["profile_pic"];
-	$profile_pic = "profile_pics/".$id."/".$profile_pic;
-	$query2 = mysqli_query($con,"UPDATE users set active = '1' WHERE id = '$id'");
-
-}
-else
-{
-	header("Location:login.php");
-}
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<meta charset="utf-8" />
+	<link rel="icon" type="image/png" href="favicon.ico">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	
+	<title>YouNited | Profile</title>
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<style>
-.fileUpload {
-    position: relative;
-    overflow: hidden;
-    margin: 10px;
-}
-.fileUpload input.upload {
-    position: absolute;
-    top: 0;
-    right: 0;
-    margin: 0;
-    padding: 0;
-    font-size: 20px;
-    cursor: pointer;
-    opacity: 0;
-    filter: alpha(opacity=0);
-}
-
-#profile_pic{
-	height:250px;
-	width:220px;
-
-}
-
-</style>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+    
+    <link href="bootstrap3/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/ct-paper.css" rel="stylesheet"/>
+    <link href="assets/css/demo.css" rel="stylesheet" /> 
+    <link href="assets/css/examples.css" rel="stylesheet" /> 
+        
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	
+	<style>
+	#map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
 </head>
-
 <body>
-<p>Welcome <b> <?php echo $name . " " . $id;?></b></p>
+    <nav class="navbar navbar-ct-transparent navbar-relative " role="navigation-demo" id="register-navbar">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <div class="logo-container">
+            <div class="logo">
+                <img src="logo.png" alt="YouNited">
+            </div>
+            <div class="brand">
+                You Nited
+            </div>
+        </div>
+        </div>
+    
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navigation-example-2">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+                <a href="#" class="btn btn-simple">NewsFeed</a>
+            </li>
+            <li>
+                <a href="#" class="btn btn-simple">Messages</a>
+            </li>
+            <li>
+                <a href="#" target="_blank" class="btn btn-simple"><i class="fa fa-twitter"></i></a>
+            </li>
+            <li>
+                <a href="#" target="_blank" class="btn btn-simple"><i class="fa fa-facebook"></i></a>
+            </li>
+           </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-->
+    </nav> 
+    
+    <div class="wrapper">
+        <div class="profile-background" style="background-image:url(bg1.jpg);"> 
+            <div class="filter-black" ></div>  
+        </div>
+        <div class="profile-content section-nude">
+            <div class="container">
+                <div class="row owner">
+                    <div class="col-md-2 col-md-offset-5 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3 text-center">
+                        <div class="avatar">
+                            <img src="faseih1.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                        </div>
+                        <div class="name">
+                            <h4>Faseih Saad<br /><small>Student</small></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 text-center">
+                        <p>An artist of considerable range, Chet Faker — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. </p>
+                        <br />
+                        <btn class="btn"><i class="fa fa-cog"></i> Edit Profile</btn>
+                    </div>
+                </div>
+                <div class="profile-tabs">
+                    <div class="nav-tabs-navigation">
+                        <div class="nav-tabs-wrapper">
+                            <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
+                                <li class="active"><a href="#friends" data-toggle="tab">Friends</a></li>
+                                <li><a href="#personal_info" data-toggle="tab">Personal Information</a></li>
+<!--                                 <li><a href="#following" data-toggle="tab">Following</a></li> -->
+                                <li><a href="#location" data-toggle="tab"  onclick = "initMap()">Location</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div id="my-tab-content" class="tab-content">
+                        <div class="tab-pane active" id="friends">
+                            <div class="row">
+                                <div class="col-md-6 col-md-offset-3">
+                                    <ul class="list-unstyled follows">
+                                        <li>
+                                            <div class="row">
+                                                <div class="col-md-2 col-md-offset-0 col-xs-3 col-xs-offset-2">
+                                                    <img src="assets/paper_img/flume.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                                                </div>
+                                                <div class="col-md-7 col-xs-4">
+                                                    <h6>Roha Asad<br /><small>Student</small></h6>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <hr />
+                                        <li>
+                                            <div class="row">
+                                                <div class="col-md-2 col-md-offset-0 col-xs-3 col-xs-offset-2">
+                                                    <img src="assets/paper_img/banks.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                                                </div>
+                                                <div class="col-md-7 col-xs-4">
+                                                    <h6>Ali Danish<br /><small>Student</small></h6>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane text-center" id="personal_info">
+                            <div class="row">
+                                <div class="col-md-2 col-md-offset-3 col-xs-3 col-xs-offset-2">
+                                <h6>Name : </h6>
+                                </div>
+                                <div class="col-md-4 col-md-offset-0 col-xs-3 col-xs-offset-2">
+                                    <p id = "pname"></p>
+                                </div>
+                            </div>
+                        <hr/>
+                        <div class="row">
+                            <div class="col-md-2 col-md-offset-3 col-xs-3 col-xs-offset-2">
+                            <h6>Occupation : </h6>
+                            </div>
+                            <div class="col-md-4 col-md-offset-0 col-xs-3 col-xs-offset-2">
+                                <p id = "poccupation"></p>
+                            </div>
+                        </div>
 
-<div class="fileUpload btn btn-primary">
-    <span>Upload Profile Pic</span>
-    <form method = "post" action = "upload_image.php" enctype = "multipart/form-data">
-    <input type="file" class="upload"  onchange="this.form.submit()" name = "profile"/>
-    <input type = "hidden" value = "<?php echo $id?>" name = "id">
-    </form>
-</div>
-<a href = "login.php?id=1"> logout</a> 
-<img src = "<?php echo $profile_pic?>" id = "profile_pic"> 
-<p>Name:<?php echo $name;?></p>
-<p>Date Of Birth:<?php echo $dob;?></p>
-<p>Email:<?php echo $email;?></p>
-<p><b>Search for people to be friends with</b></p>
-<form> 
-<input type="text" onkeyup="showHint(this.value)">
-</form>
-<p>Suggestions: <span id="txtHint"></span></p>
-<p><a href = "home.php">News Feed</a></p>
+                        <hr/>
+                        <div class="row">
+                            <div class="col-md-2 col-md-offset-3 col-xs-3 col-xs-offset-2">
+                            <h6>Address : </h6>
+                            </div>
+                            <div class="col-md-4 col-md-offset-0 col-xs-3 col-xs-offset-2">
+                                <p id = "paddress"></p>
+                            </div>
+                        </div>
 
 
+                        <hr/>
+                        <div class="row">
+                            <div class="col-md-2 col-md-offset-3 col-xs-3 col-xs-offset-2">
+                            <h6>City : </h6>
+                            </div>
+                            <div class="col-md-4 col-md-offset-0 col-xs-3 col-xs-offset-2">
+                                <p id = "pcity"></p>
+                            </div>
+                        </div>
 
-<!-- javascript -->
+                        <hr/>
+                        <div class="row">
+                            <div class="col-md-2 col-md-offset-3 col-xs-3 col-xs-offset-2">
+                            <h6>Age : </h6>
+                            </div>
+                            <div class="col-md-4 col-md-offset-0 col-xs-3 col-xs-offset-2">
+                                <p id = "page"></p>
+                            </div>
+                        </div>
 
-<script>
-function showHint(str) {
-    if (str.length == 0) { 
-        document.getElementById("txtHint").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "search.php?q=" + str, true);
-        xmlhttp.send();
-    }
-}
-</script>
+
+                        <hr/>
+                        <div class="row">
+                            <div class="col-md-2 col-md-offset-3 col-xs-3 col-xs-offset-2">
+                            <h6>Birthday : </h6>
+                            </div>
+                            <div class="col-md-4 col-md-offset-0 col-xs-3 col-xs-offset-2">
+                                <p id = "pbirthday"></p>
+                            </div>
+                        </div>
+
+
+                        <hr/>
+                        <div class="row">
+                            <div class="col-md-2 col-md-offset-3 col-xs-3 col-xs-offset-2">
+                            <h6>Institute : </h6>
+                            </div>
+                            <div class="col-md-4 col-md-offset-0 col-xs-3 col-xs-offset-2">
+                                <p id = "pinstitute"></p>
+                            </div>
+                        </div>
+
+                        </div>
+						
+                        <div class="tab-pane text-center" id="location">
+                                <div id = "map"></div>
+                        </div>
+						
+						
+
+                    </div>
+
+                </div>        
+            </div>
+        </div>
+    </div> 
+    <footer class="footer-demo section-nude">
+        <div class="container">
+            <nav class="pull-left">
+                <ul>
+    
+                    <li>
+                        <h5>Web Engineering Project</h5>
+                    </li>
+                </ul>
+            </nav>
+            <div class="copyright pull-right">
+                &copy; BESE-6B Students
+            </div>
+        </div>
+    </footer>
+
+    
 </body>
+
+<script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+<script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+
+<script src="bootstrap3/js/bootstrap.js" type="text/javascript"></script>
+
+<!--  Plugins -->
+<script src="assets/js/ct-paper-checkbox.js"></script>
+<script src="assets/js/ct-paper-radio.js"></script>
+<script src="assets/js/bootstrap-select.js"></script>
+<script src="assets/js/bootstrap-datepicker.js"></script>
+
+<script src="assets/js/ct-paper.js"></script>
+<script>
+      function initMap() {
+        var uluru = {lat: 33.738045 , lng: 73.084488};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+	  
+	  $(document).ready(function(){
+	  
+		$.getJSON("files/data.json", function(result){
+			var x = result["users_info"][0];
+			
+			$('#pname').text(x["name"]);
+			$('#poccupation').text(x["occupation"]);
+			$('#pcity').text(x["city"]);
+			$('#paddress').text(x["address"]);
+			$('#page').text(x["age"]);
+			$('#pbirthday').text(x["birthday"]);
+			$('#pinstitute').text(x["institute"]);
+		});
+		
+	  });
+ </script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZLY5GlEOuGb1vl6DuWELBzSGvL7jVXFA&callback=initMap" type="text/javascript"></script>
 </html>
