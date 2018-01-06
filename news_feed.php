fd1a5b29e44ad3660ec1e751ef0e85cc9ea261c6
Faseih Saad <fsaad.bese15seecs@seecs.edu.pk>
2018-01-07 00:54:19 +0500

    Some changes

--- news_feed.php
+++ news_feed.php
@@ -80,7 +80,7 @@ else
                   <div class="collapse navbar-collapse" id="navigation-example-2">
                     <ul class="nav navbar-nav">
                             <li class="dropdown">
-                                    <button id="acha" href="#" type="button" style="color:white;" class="dropdown-toggle btn btn-info" data-toggle="dropdown">Faseih Saad<span class="caret"></span></button>
+                                    <button id="acha" href="#" type="button" style="color:white;" class="dropdown-toggle btn btn-info" data-toggle="dropdown"><?php echo ucwords($_SESSION['name'])?><span class="caret"></span></button>
 <!--                                  You can add classes for different colours on the next element -->
 <script>
     
@@ -387,7 +387,7 @@ function checkSubmit(e,x) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("test").innerHTML = this.responseText;
+      //document.getElementById("test").innerHTML = this.responseText;
       }
       };
       xhttp.open("GET", "message.php?msg=" + message+"&id=" + id, true);
